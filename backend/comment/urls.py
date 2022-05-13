85fb0d1e54d8a3a3f3f0e60ad92f6d588262fc81
ruben rodriguez <rotap45r@gmail.com>
2022-05-13 18:01:45 -0400

    made a little more headway with our views and urls

--- backend/comment/urls.py
+++ backend/comment/urls.py
@@ -2,7 +2,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.comment_list),
-    path('comment/<int:pk>/', views.comment_detail),
-        
+    path('', views.comment_list),            # GET all comments (No Auth)
+    # GET single Comment (Auth Required) (PUT, DELETE)
+    path('<int:pk>/', views.comment_detail),
+    # Post a new record (Auth required)
+    path('add/', views.add_record),
+
 ]
--- backend/comment/views.py
+++ backend/comment/views.py
@@ -2,17 +2,17 @@
 from rest_framework.response import Response
 from rest_framework import status
 from django.shortcuts import get_object_or_404
-from .serializers import CommentSerializer 
+from .serializers import CommentSerializer
 from .models import Comment
 from rest_framework.permissions import IsAuthenticated, AllowAny
 from rest_framework.decorators import api_view, permission_classes
 
-#Create your views here.
+# Create your views here.
 
 # @api_view(['GET', 'POST'])
 # def comment_list(request):
 
-    
+
 #     if request.method == 'GET':
 #         comment = Comment.objects.all()
 #         serializer = CommentSerializer(comment, many=True)
@@ -47,7 +47,6 @@ from rest_framework.decorators import api_view, permission_classes
 #         return Response(custom_response, status=status.HTTP_200_OK)
 
 
-
 @api_view(['GET'])
 @permission_classes([AllowAny])
 def comment_list(request):
@@ -56,21 +55,20 @@ def comment_list(request):
     return Response(serializer.data)
 
 
-@api_view(['GET', 'POST'])
+@api_view(['GET', 'DELETE'])
 @permission_classes([IsAuthenticated])
-def comment_detail (request):
+def comment_detail(request, pk):
     print(
         'User ', f"{request.user.id} {request.user.email} {request.user.username}")
-    if request.method == 'POST':
-        serializer = CommentSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save(user=request.user)
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-    elif request.method == 'GET':
-        comment = Comment.objects.filter(user_id=request.user.id)
+
+    if request.method == 'GET':
+        comment = Comment.objects.filter(user_id=request.user.id, pk=pk)
         serializer = CommentSerializer(comment, many=True)
         return Response(serializer.data)
+    elif request.method == 'DELETE':
+        comment.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
 
 @api_view(['POST'])
 @permission_classes([IsAuthenticated])
