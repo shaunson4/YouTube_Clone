86f3e323dbf8e823f21e139d16ba37b28016bcdf
ruben rodriguez <rotap45r@gmail.com>
2022-05-17 12:09:04 -0400

    created a form for the searchpage.jsx with name,lastname, and email address

--- frontend/src/components/SearchPage/SearchPage.jsx
+++ frontend/src/components/SearchPage/SearchPage.jsx
@@ -4,22 +4,30 @@ import { useState } from "react";
 
 
 const SearchPage=(props)=>{
-    const [videos, setVideos] = useState([]);
+    const [videos, setVideos, getVideo] = useState([]);
     //add searchTerm variable
 
     const fetchVideos = async () => {
         try {
             //use string interpolation to insert searchTerm to the axios url string
-          let response = await axios.get("",);
+          let response = await axios.get("https://www.googleapis.com/youtube/v3/search?q=pizza&key=AIzaSyCIYSGY1-KiGgmb3f6F1FthlDXC0RhG9V8", getVideo);
           setVideos(response.data.items);
         } catch (error) {
           console.log(error.message);
         }
       };
-//build form in the return, based off rReact Forms lecture
+//build form in the return, based off React Forms lecture
     return(
       <div>{console.log('"videos" in render', videos)}
       <button onClick={fetchVideos}>Click to search</button>
+    <form action="" method="">
+        <label for="first_name">First Name:</label>
+        <input type="text" name="first_name"></input>
+        <label for="last_name">Last Name:</label>
+        <input type="text" name="last_name"></input>
+        <label for="email">Email Address:</label>
+        <input type="email" name="email"></input>
+            </form>
       </div>
   )
 
